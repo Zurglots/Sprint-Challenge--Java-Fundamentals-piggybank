65f7973717004c3ac94c3473d95131fde6f8c0a6
Zurglots <bfeole@gmail.com>
2019-07-12 11:48:07 -0400

    Money objs added to ArrayList printing total

--- src/piggybank/Main.java
+++ src/piggybank/Main.java
@@ -11,9 +11,24 @@ public class Main
 	{
 		DecimalFormat fp = new DecimalFormat("$###,###.00");
 
+		ArrayList<Money> piggyBank = new ArrayList<>();
+
+		piggyBank.add(new Quarter());
+        piggyBank.add(new Dime());
+        piggyBank.add(new Dollar(5));
+        piggyBank.add(new Nickel(3));
+        piggyBank.add(new Dime(7));
+        piggyBank.add(new Dollar());
+		piggyBank.add(new Penny(10));
+
+		double total = 0;
+		for(Money m : piggyBank)
+		{
+			total += m.getValue();
+		}
 
 		// Example sout from read me.
-		// System.out.println("The piggy bank holds " + fp.format(myValue));
+		System.out.println("The piggy bank holds " + fp.format(total));
 	}
 
 
--- src/piggybank/Money.java
+++ src/piggybank/Money.java
@@ -10,5 +10,4 @@ public abstract class Money
 	public abstract String getName();
 	public abstract double getValue();
 
-
 }
